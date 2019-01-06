102d6f45a76e6509714b706f569e03a259226da7
oonderstanbable <45098109+oonderstanbable@users.noreply.github.com>
2019-01-06 09:04:33 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const Discord = require('./colors.json');
 
 client.on('ready', function(){
     console.log('ExtraBot!');
